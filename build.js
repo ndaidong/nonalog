c8977589a04ccb046b6dba8195011cac1bc2b4ff
Dong Nguyen <ndaidong@gmail.com>
2022-08-19 16:43:35 +0700

    v1.0.1 - Support CJS
    - Build cjs version for old environment

---
+++ build.js
@@ -0,0 +1,63 @@
+/**
+ * build.js
+ * @ndaidong
+ **/
+
+import { readFileSync, writeFileSync, rmSync, mkdirSync } from 'fs'
+
+import { buildSync } from 'esbuild'
+
+const pkg = JSON.parse(readFileSync('./package.json', { encoding: 'utf-8' }))
+
+rmSync('dist', {
+  force: true,
+  recursive: true
+})
+mkdirSync('dist')
+
+const buildTime = (new Date()).toISOString()
+const comment = [
+  `// ${pkg.name}@${pkg.version}, by ${pkg.author}`,
+  `built with esbuild at ${buildTime}`,
+  `published under ${pkg.license} license`
+].join(' - ')
+
+/**
+ * @type {import('esbuild').BuildOptions}
+ * */
+const baseOpt = {
+  entryPoints: ['main.js'],
+  bundle: true,
+  charset: 'utf8',
+  target: ['es2020', 'node14'],
+  minify: true,
+  write: true,
+  sourcemap: 'external',
+  external: ['canvas']
+}
+
+/**
+ * @type {import('esbuild').BuildOptions}
+ */
+const cjsVersion = {
+  ...baseOpt,
+  platform: 'node',
+  format: 'cjs',
+  mainFields: ['main'],
+  outfile: `dist/cjs/${pkg.name}.js`,
+  banner: {
+    js: comment
+  }
+}
+buildSync(cjsVersion)
+
+const cjspkg = {
+  name: pkg.name + '-cjs',
+  version: pkg.version,
+  main: `./${pkg.name}.js`
+}
+writeFileSync(
+  'dist/cjs/package.json',
+  JSON.stringify(cjspkg, null, '  '),
+  'utf8'
+)
